e129786c32361a8161748c2c74a4b86ee9e2878c
Ayush Bajpai <76244712+MrAyushBajpai@users.noreply.github.com>
2023-12-01 12:51:34 +0530

    Added Gradient Hover Effect

---
+++ gradient-hover.js
@@ -0,0 +1,9 @@
+const root = document.documentElement;
+ 
+document.addEventListener('mousemove', evt => {
+    let x = evt.clientX / innerWidth;
+    let y = evt.clientY / innerHeight;
+ 
+    root.style.setProperty('--mouse-x', x);
+    root.style.setProperty('--mouse-y', y);
+});
